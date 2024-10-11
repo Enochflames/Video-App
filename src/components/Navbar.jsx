203d8fc1ffcd33c4e22e8b39e9d8a69387f1306f
Enochflames <adebayo.enochobaloluwa@gmail.com>
2024-10-11 01:18:17 +0100

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#CC5500', top: 0, justifyContent: "space-between"}}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
-      <h1>Enoch's Video App</h1>
+      <h1>Video App</h1>
     </Link>
     <SearchBar />
   </Stack>
